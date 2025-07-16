94d4d8208cd38b59e3622577b54c467a5048e391
Samad <samadmalikz123@example.com>
2025-07-16 20:02:21 +0530

    index change

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
-const router = require("./routes");
 const dotenv = require("dotenv");
-dotenv.config();
 const connectDatabase = require("./config/database");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+dotenv.config();
 const app = express();
-app.use(express.json());
-app.use(cookieParser())
 connectDatabase();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api", router);
-const port = process.env.PORT;
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server connected on : ${port}`);
