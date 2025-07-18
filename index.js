a42fa105c218b42ef94dcabc0246344b25886a13
Samad <samadmalikz123@example.com>
2025-07-18 13:05:35 +0530

    add cors

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ dotenv.config();
 const app = express();
 connectDatabase();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,5 +23,5 @@ app.use("/api", router);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`server connected on : ${port}`);
+  console.log(`server connected on : ${port}`);
 });
